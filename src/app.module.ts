0e1b72d2802ca54237bf0616201d372ff6e5c11d
Sage Anne <sageanneacol4-wq@users.noreply.github.com>
2026-02-21 11:05:08 +0800

    Update database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,33 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
+const dbUrl =
+  process.env.MYSQL_PUBLIC_URL ||
+  process.env.DATABASE_URL;
+
+if (!dbUrl) {
+  console.error('❌ Database URL is not defined!');
+  throw new Error('Database URL is not defined!');
+}
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      url: process.env.MYSQL_PUBLIC_URL, // match Railway variable
+      url: dbUrl,
       autoLoadEntities: true,
       synchronize: true,
+      ssl:
+        process.env.NODE_ENV === 'production'
+          ? { rejectUnauthorized: false }
+          : false,
     }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
       serveRoot: '/',
     }),
+
     UsersModule,
   ],
   controllers: [AppController],
