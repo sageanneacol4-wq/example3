638d44a9a92893b3d5b8b927d112de96d7b35bd7
Sage Anne <sageanneacol4-wq@users.noreply.github.com>
2026-02-22 09:57:49 +0800

    Update DB connection to use shared variables

--- library/server.js
+++ library/server.js
@@ -13,11 +13,11 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: 3306
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306
 });
 
 db.connect(err => {
